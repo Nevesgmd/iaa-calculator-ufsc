27a4ac4c397794eab58fc36222028c16134604ea
nevesgmd <gmedeirosneves@gmail.com>
2020-05-10 18:58:39 -0300

    docs: code commentaries

--- app/iaa_calc_gui.py
+++ app/iaa_calc_gui.py
@@ -1,3 +1,4 @@
+# Imports
 from scraper.ufsc_scraper import UfscScraper
 from student.student import Student
 from kivy.app import App
@@ -5,9 +6,10 @@ from kivy.lang import Builder
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
-Window.clearcolor = (0.85, 0.85, 0.9, 0.8)
-Window.size = (800, 460)
-Window.minimum_width, Window.minimum_height = (700, 400)
+
+Window.clearcolor = (0.85, 0.85, 0.9, 0.8)  # Defining background color
+Window.size = (800, 460)  # Defining initial window size
+Window.minimum_width, Window.minimum_height = (700, 400)  # Defining mininum window size
 
 
 class PageManager(ScreenManager):
@@ -27,7 +29,12 @@ class InvalidUserPage(Screen):
 
 
 class HomePage(Screen):
-    def create_x_text_inputs(self, placeholders):
+    def create_x_text_inputs(self, placeholders: list):
+        """
+        Creates a number of text inputs equals to the number of current classes of the student
+        :param placeholders: current classes names to display as placeholders in the text inputs
+        :return: None
+        """
         gap = -0.07
         for i, placeholder in enumerate(placeholders):
             text_input = TextInput(font_size=30-(len(placeholders)-5)*3.1,
@@ -72,6 +79,7 @@ class IaaCalculator(App):
         return kv
 
     def _on_keyboard_down(self, instance, keyboard, keycode, text, modifiers):
+        """Identifies when Enter is pressed and run different functions depending on the page (submit text inputs)"""
         if keycode == 40 and kv.current == 'login':  # 40 - Enter key pressed
             self.get_login_values()
             self.update_home_page()
@@ -80,6 +88,7 @@ class IaaCalculator(App):
             self.clean_text_inputs()
 
     def get_login_values(self):
+        """Logs the user into the CAGR with the values ​​entered, then clears the text inputs"""
         user = self.__login_page.ids.login.text
         password = self.__login_page.ids.password.text
         self.__student.user_browser = self.__scraper.login(user, password)
@@ -88,6 +97,12 @@ class IaaCalculator(App):
         self.__login_page.ids.password.text = str()
 
     def validate_student_data(self):
+        """
+        Get student name, grades, indices and current classes if student user and password are valid,
+        Leads to a error screen if the student registration does not exist or the CAGR is down.
+        Leads to a congratulations screen if the student is already graduated.
+        :return: True if user and password are valid, None otherwise
+        """
         try:
             student_data = self.__scraper.get_student_data(self.__student.user_browser)
             self.__student.name = student_data['name']
@@ -107,6 +122,7 @@ class IaaCalculator(App):
             kv.transition.direction = "down"
 
     def update_home_page(self):
+        """Leads to updated home page with student data"""
         if self.validate_student_data():
             current_classes_names = [current_class[0] for current_class in self.__student.current_classes]
 
@@ -120,6 +136,11 @@ class IaaCalculator(App):
             kv.transition.direction = "left"
 
     def update_new_indices(self):
+        """
+        Updates and leads the user to the new_indices_page if provided grades are valid,
+        leads to an error screen otherwise
+        :return: None
+        """
         try:
             possible_grades = [self.__home_page.ids['text_input_{}'.format(i)].text
                                for i in range(len(self.__student.current_classes))]
@@ -141,6 +162,7 @@ class IaaCalculator(App):
             kv.transition.direction = "up"
 
     def clean_text_inputs(self):
+        """Clean text inputs on home page"""
         for i in range(len(self.__student.current_classes)):
             self.__home_page.ids['text_input_{}'.format(i)].text = ''
 
